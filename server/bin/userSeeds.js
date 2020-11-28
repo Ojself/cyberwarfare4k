e8cfe5a23adb7d846f303091d512a30fa10aaea4
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-11-28 14:27:12 +0100

    Copied hackeravatars to bin

--- server/bin/userSeeds.js
+++ server/bin/userSeeds.js
@@ -21,7 +21,9 @@ let avatars;
 /* https://stackoverflow.com/questions/2727167/how-do-you-get-a-list-of-the-names-of-all-files-present-in-a-directory-in-node-j */
 async function walk(dir) {
   let files = await fs.readdir(dir);
+  console.log(files, 'files');
   files = await Promise.all(files.map(async (file) => {
+    console.log(file, 'file');
     const filePath = path.join(dir, file);
     const stats = await fs.stat(filePath);
     if (stats.isDirectory()) return walk(filePath);
