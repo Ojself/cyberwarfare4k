ccecf3b90183f4c7d1e275a299367c4e70b922a6
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-11-28 13:38:05 +0100

    Copied hackeravatars to bin

--- server/bin/userSeeds.js
+++ server/bin/userSeeds.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs').promises;
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
 require('../configs/database');
 
@@ -14,10 +15,22 @@ const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const City = require('../models/City');
 const Alliance = require('../models/Alliance');
-const { walk } = require('./avatars');
 
 let avatars;
-const pathToAvatars = '../../client/public/hackerAvatars';
+const pathToAvatars = './hackerAvatars';
+
+/* https://stackoverflow.com/questions/2727167/how-do-you-get-a-list-of-the-names-of-all-files-present-in-a-directory-in-node-j */
+async function walk(dir) {
+  let files = await fs.readdir(dir);
+  files = await Promise.all(files.map(async (file) => {
+    const filePath = path.join(dir, file);
+    const stats = await fs.stat(filePath);
+    if (stats.isDirectory()) return walk(filePath);
+    if (stats.isFile()) return filePath;
+  }));
+
+  return files.reduce((all, folderContents) => all.concat(folderContents), []);
+}
 
 /* Finds all avatars */
 walk(pathToAvatars).then((result) => {
