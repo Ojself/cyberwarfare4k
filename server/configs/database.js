d83c7edc8ee75b845a1acd0f2d4fa7721d0a22c5
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-12-05 13:12:37 +0100

    fix battery interval

--- server/configs/database.js
+++ server/configs/database.js
@@ -9,14 +9,20 @@ const currencyPriceInterval = require('../cronjobs/currencyPriceInterval');
 const dataCenterPayoutInterval = require('../cronjobs/dataCenterPayoutInterval');
 const cityPriceInterval = require('../cronjobs/cityPriceInterval');
 
-const batteryJob = new CronJob('2 */10 * * * *', (() => {
+const batteryJob = new CronJob('2 */0,10,20,30,40,50 * * * *', (() => {
   console.log('batteryJob started');
   batteryInterval();
 }), null, true, 'America/Los_Angeles');
 
-const currencyPriceJob = new CronJob('10 0 * * * *', (() => {
+const currencyPriceJob = new CronJob('6 0 * * * *', (() => {
   console.log('currencyPriceJob started');
   currencyPriceInterval();
+}), null, true, 'America/Los_Angeles');
+
+const bonusBatteryJob = new CronJob('10 0 * * * *', (() => {
+  console.log('bonusBatteryJob started');
+  console.log(new Date(Date.now()));
+  bonusBatteryInterval();
 }), null, true, 'America/Los_Angeles');
 
 const stashPriceJob = new CronJob('15 59 * * * *', (() => {
@@ -32,12 +38,6 @@ const dataCenterPayoutJob = new CronJob('30 * * * * *', (() => {
 const cityMultiplierJob = new CronJob('45 59 * * * *', (() => {
   console.log('cityMultiplierJob started');
   cityPriceInterval();
-}), null, true, 'America/Los_Angeles');
-
-const bonusBatteryJob = new CronJob('59 0 * * * *', (() => {
-  console.log('bonusBatteryJob started');
-  console.log(new Date(Date.now()));
-  bonusBatteryInterval();
 }), null, true, 'America/Los_Angeles');
 
 const uri = process.env.MONGODB_URI
