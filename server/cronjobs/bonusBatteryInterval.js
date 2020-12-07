5f887f8a2d9a238a224a6bba6f1e0edecd9f4252
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-12-07 10:31:13 +0100

    Print interval info

--- server/cronjobs/bonusBatteryInterval.js
+++ server/cronjobs/bonusBatteryInterval.js
@@ -9,12 +9,24 @@ const subscriptionBonus = {
 
 /* Ensures that users gets more battery after cetain time and never exceeds 100 */
 const bonusBatteryInterval = async () => {
-  const users = await User.find({ $or: [{ 'account.subscription': 'Bronze' }, { 'account.subscription': 'Silver' }, { 'account.subscription': 'Gold' }, { 'account.subscription': 'Platinum' }] });
+  const users = await User.find({
+    $or: [
+      { "account.subscription": "Bronze" },
+      { "account.subscription": "Silver" },
+      { "account.subscription": "Gold" },
+      { "account.subscription": "Platinum" },
+      { "earnBattery.githubStar": true },
+    ],
+  });
+  console.log(users.length,'length')
   users.forEach((user) => {
     const { subscription } = user.account || 'Bronze';
+    console.log(subscription, "subscription");
     const githubBonus = user.earnBattery.githubStar ? 1 : 0;
     user.playerStats.battery += subscriptionBonus[subscription].battery + githubBonus;
+    console.log(user.playerStats.battery, "user.playerStats.battery");
     if (user.playerStats.battery > subscriptionBonus[subscription].max) {
+      console.log("if!", subscriptionBonus[subscription].max);
       user.playerStats.battery = subscriptionBonus[subscription].max;
     }
   });
