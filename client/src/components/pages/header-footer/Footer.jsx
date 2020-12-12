d3fae2bf1f26b7c7a5f28468d8d340f1f3496dae
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-12-13 00:06:33 +0100

    add database reset note

--- client/src/components/pages/header-footer/Footer.jsx
+++ client/src/components/pages/header-footer/Footer.jsx
@@ -3,11 +3,25 @@ import React from "react";
 const Footer = () => {
   const currentYear = new Date(Date.now()).getFullYear();
   return (
-    <footer style={{alignSelf:"flex-end", bottom:"0"}} className="w-100 dark-light mt-5">
+    <footer
+      style={{ alignSelf: "flex-end", bottom: "0" }}
+      className="w-100 dark-light mt-5"
+    >
       <div className="text-center py-3">
-        © {currentYear} Copyright
+        <p className="text-warning" style={{ fontSize: "0.75rem" }}>
+          Warning: The server and database will be reset{" "}
+          <strong>17. December 19:00 CET </strong>
+        </p>
+        {/* © {currentYear} Copyright */}
         <div className="py-2">
-          <a target="_blank" rel="noopener noreferrer" href="http://flesjoe.com/">Flesjoe.com</a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/Ojself/cyberwarfare4k"
+          >
+            CyberhackerWarfare4000
+          </a>{" "}
+          0.1.0
         </div>
       </div>
     </footer>
@@ -15,3 +29,4 @@ const Footer = () => {
 };
 
 export default Footer;
+
