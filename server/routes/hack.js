80e5d7f24ed240e113222e39f89a692e23faed66
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-12-17 17:33:17 +0100

    Fix disallow

--- server/routes/hack.js
+++ server/routes/hack.js
@@ -168,7 +168,7 @@ router.post('/:opponentId', async (req, res) => {
   const now = Date.now();
 
   const userIsOnline = await getOnlineUsers(opponent._id.toString());
-/* 
+
   const disallowed = await attackRouteCriterias(user, opponent, batteryCost, now, userIsOnline);
 
   if (disallowed) {
@@ -177,7 +177,7 @@ router.post('/:opponentId', async (req, res) => {
       message: disallowed,
     });
   }
- */
+
   const finalResult = await fightHacker(user, opponent, batteryCost, now, userIsOnline);
 
   const updatedUser = await saveAndUpdateUser(finalResult.user);
