f8bcc50fd8e7c42b39cd35b1562661b6ad669585
Tormod Flesjø <tormod.flesjo@gmail.com>
2021-01-24 21:38:59 +0100

    Add terms of sale

--- client/src/components/pages/earnBattery/TermsOfSale.jsx
+++ client/src/components/pages/earnBattery/TermsOfSale.jsx
@@ -2,12 +2,445 @@ import React from "react";
 
 function TermsOfSale() {
   return (
-    <div>
-      <h1>Terms of sale</h1>
-      <p>Work in progress! (25th January 2021)</p>
-      <a href="https://www.forbrukertilsynet.no/lov-og-rett/veiledninger-og-retningslinjer/standard-salgsbetingelser-for-forbrukerkjop-av-varer-over-internett">
-        Salgsvilkår
-      </a>
+    <div className="text-left container">
+      <h1 className="mb-5 text-center">
+        Standard salgsbetingelser for forbrukerkjøp av varer over Internett
+      </h1>
+
+      <section>
+        <h2>Innledning</h2>
+
+        <div>
+          <p>
+            Dette kjøpet er regulert av de nedenstående standard
+            salgsbetingelser for forbrukerkjøp av varer over Internett.
+            Forbrukerkjøp over internett reguleres hovedsakelig av avtaleloven,
+            forbrukerkjøpsloven, markedsføringsloven, angrerettloven og
+            ehandelsloven, og disse lovene gir forbrukeren ufravikelige
+            rettigheter. Lovene er tilgjengelig på{" "}
+            <a target="_blank" href="http://www.lovdata.no/" rel="noopener">
+              www.lovdata.no.
+            </a>{" "}
+            Vilkårene i denne avtalen skal ikke forstås som noen begrensning i
+            de lovbestemte rettighetene, men oppstiller partenes viktigste
+            rettigheter og plikter for handelen.
+          </p>
+          <p>
+            Salgsbetingelsene er utarbeidet og anbefalt av Forbrukertilsynet.{" "}
+            <a href="https://forbrukertilsynet.no/lov-og-rett/veiledninger-og-retningslinjer/veiledning-standard-salgsbetingelser-forbrukerkjop-varer-internett">
+              For en bedre forståelse av disse salgsbetingelsene, se
+              Forbrukertilsynets veileder her.{" "}
+            </a>
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h2>1. Avtalen</h2>
+
+        <div>
+          <p>
+            Avtalen består av disse salgsbetingelsene, opplysninger gitt i
+            bestillingsløsningen og eventuelt særskilt avtalte vilkår. Ved
+            eventuell motstrid mellom opplysningene, går det som særskilt er
+            avtalt mellom partene foran, så fremt det ikke strider mot
+            ufravikelig lovgivning.
+          </p>
+          <p>
+            Avtalen vil i tillegg bli utfylt av relevante lovbestemmelser som
+            regulerer kjøp av varer mellom næringsdrivende og forbrukere.
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h2>2. Partene</h2>
+
+        <div>
+          <p>
+            Selger er Flesjø Import, Gøteborggata 20, 0556 Oslo,
+            tormod.flesjo@gmail.com, +4798999898, 913423526, og betegnes i det
+            følgende som &nbsp;selger/selgeren.
+          </p>
+          <p>
+            Kjøper er den forbrukeren som foretar bestillingen, og betegnes i
+            det følgende som &nbsp;kjøper/kjøperen.
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h2>3. Pris</h2>
+
+        <div>
+          <p>
+            Den oppgitte prisen for varen og tjenester er den totale prisen
+            kjøper skal betale. Denne prisen inkluderer alle avgifter og
+            tilleggskostnader. Ytterligere kostnader som selger før kjøpet ikke
+            har informert om, skal kjøper ikke bære.
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h2>4. Avtaleinngåelse</h2>
+
+        <div>
+          <p>
+            Avtalen er bindende for begge parter når kjøperen har sendt sin
+            bestilling til selgeren.
+          </p>
+          <p>
+            Avtalen er likevel ikke bindende hvis det har forekommet skrive-
+            eller tastefeil i tilbudet fra selgeren i bestillingsløsningen i
+            nettbutikken eller i kjøperens bestilling, og den annen part innså
+            eller burde ha innsett at det forelå en slik feil.
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h2>5. Betalingen</h2>
+
+        <div>
+          <p>
+            Selgeren kan kreve betaling for varen fra det tidspunkt den blir
+            sendt fra selgeren til kjøperen.
+          </p>
+          <p>
+            Dersom kjøperen bruker kredittkort eller debetkort ved betaling, kan
+            selgeren reservere kjøpesummen på kortet ved bestilling. Kortet blir
+            belastet samme dag som varen sendes.
+          </p>
+          <p>
+            Ved betaling med faktura, blir fakturaen til kjøperen utstedt ved
+            forsendelse av varen. Betalingsfristen fremgår av fakturaen og er på
+            minimum 14 dager fra mottak.
+          </p>
+          <p>Kjøpere under 18 år kan ikke betale med etterfølgende faktura.</p>
+        </div>
+      </section>
+
+      <section>
+        <h2>6. Levering</h2>
+
+        <div>
+          <p>
+            Levering er skjedd når kjøperen, eller hans representant, har
+            overtatt tingen.
+          </p>
+          <p>
+            Hvis ikke leveringstidspunkt fremgår av bestillingsløsningen, skal
+            selgeren levere varen til kjøper uten unødig opphold og senest 30
+            dager etter bestillingen fra kunden. Varen skal leveres hos kjøperen
+            med mindre annet er særskilt avtalt mellom partene.
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h2>7. Risikoen for varen</h2>
+
+        <div>
+          <p>
+            Risikoen for varen går over på kjøper når han, eller hans
+            representant, har fått varene levert i tråd med punkt 6.
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h2>8. Angrerett</h2>
+
+        <div>
+          <p>
+            Med mindre avtalen er unntatt fra angrerett, kan kjøperen angre
+            kjøpet av varen i henhold til angrerettloven.
+          </p>
+          <p>
+            Kjøperen må gi selger melding om bruk av angreretten innen 14 dager
+            fra fristen begynner å løpe. I fristen inkluderes alle
+            kalenderdager. Dersom fristen ender på en lørdag, helligdag eller
+            høytidsdag forlenges fristen til nærmeste virkedag.
+          </p>
+          <p>
+            Angrefristen anses overholdt dersom melding er sendt før utløpet av
+            fristen. Kjøper har bevisbyrden for at angreretten er blitt gjort
+            gjeldende, og meldingen bør derfor skje skriftlig (angrerettskjema,
+            e-post eller brev).
+          </p>
+          <p>Angrefristen begynner å løpe:</p>
+          <ul>
+            <li>
+              Ved kjøp av enkeltstående varer vil angrefristen løpe fra dagen
+              etter varen(e) er mottatt.
+            </li>
+            <li>
+              Selges et abonnement, eller innebærer avtalen regelmessig levering
+              av identiske varer, løper fristen fra dagen etter første
+              forsendelse er mottatt.
+            </li>
+            <li>
+              Består kjøpet av flere leveranser, vil angrefristen løpe fra dagen
+              etter siste leveranse er mottatt.
+            </li>
+          </ul>
+          <p>
+            Angrefristen utvides til 12 måneder etter utløpet av den
+            opprinnelige fristen dersom selger ikke før avtaleinngåelsen
+            opplyser om at det foreligger angrerett og standardisert
+            angreskjema. Tilsvarende gjelder ved manglende opplysning om vilkår,
+            tidsfrister og fremgangsmåte for å benytte angreretten. Sørger den
+            næringsdrivende for å gi opplysningene i løpet av disse 12 månedene,
+            utløper angrefristen likevel 14 dager etter den dagen kjøperen
+            mottok opplysningene.
+          </p>
+          <p>
+            Ved bruk av angreretten må varen leveres tilbake til selgeren uten
+            unødig opphold og senest 14 dager fra melding om bruk av angreretten
+            er gitt. Kjøper dekker de direkte kostnadene ved å returnere varen,
+            med mindre annet er avtalt eller selger har unnlatt å opplyse om at
+            kjøper skal dekke returkostnadene. Selgeren kan ikke fastsette gebyr
+            for kjøperens bruk av angreretten.
+          </p>
+          <p>
+            Kjøper kan prøve eller teste varen på en forsvarlig måte for å
+            fastslå varens art, egenskaper og funksjon, uten at angreretten
+            faller bort. Dersom prøving eller test av varen går utover hva som
+            er forsvarlig og nødvendig, kan kjøperen bli ansvarlig for eventuell
+            redusert verdi på varen.
+          </p>
+          <p>
+            Selgeren er forpliktet til å tilbakebetale kjøpesummen til kjøperen
+            uten unødig opphold, og senest 14 dager fra selgeren fikk melding om
+            kjøperens beslutning om å benytte angreretten. Selger har rett til å
+            holde tilbake betalingen til han har mottatt varene fra kjøperen,
+            eller til kjøper har lagt frem dokumentasjon for at varene er sendt
+            &nbsp;tilbake.
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h2>
+          9. Forsinkelse og manglende levering - kjøpernes rettigheter og frist
+          for å melde krav
+        </h2>
+
+        <div>
+          <p>
+            Dersom selgeren ikke leverer varen eller leverer den for sent i
+            henhold til avtalen mellom partene, og dette ikke skyldes kjøperen
+            eller forhold på kjøperens side, kan kjøperen i henhold til reglene
+            i forbrukerkjøpslovens kapittel 5 etter omstendighetene{" "}
+            <em>holde kjøpesummen tilbake</em>, kreve <em>oppfyl</em>
+            <em>lelse</em>, <em>heve </em>avtalen og/eller kreve{" "}
+            <em>erstatning </em>fra selgeren.
+          </p>
+          <p>
+            Ved krav om misligholdsbeføyelser bør meldingen av bevishensyn være
+            skriftlig (for eksempel e-post).
+          </p>
+          <h3>Oppfyllelse</h3>
+          <p>
+            Kjøper kan fastholde kjøpet og kreve oppfyllelse fra selger. Kjøper
+            kan imidlertid ikke kreve oppfyllelse dersom det foreligger en
+            hindring som selgeren ikke kan overvinne, eller dersom oppfyllelse
+            vil medføre en så stor ulempe eller kostnad for selger at det står i
+            vesentlig misforhold til kjøperens interesse i at selgeren
+            oppfyller. Skulle vanskene falle bort innen rimelig tid, kan kjøper
+            likevel kreve oppfyllelse.
+          </p>
+          <p>
+            Kjøperen taper sin rett til å kreve oppfyllelse om han eller hun
+            venter urimelig lenge med å fremme kravet.
+          </p>
+          <h3>Heving</h3>
+          <p>
+            Dersom selgeren ikke leverer varen på leveringstidspunktet, skal
+            kjøperen oppfordre selger til å levere innen en rimelig
+            tilleggsfrist for oppfyllelse. Dersom selger ikke leverer varen
+            innen tilleggsfristen, kan kjøperen heve kjøpet.
+          </p>
+          <p>
+            Kjøper kan imidlertid heve kjøpet umiddelbart hvis selger nekter å
+            levere varen. Tilsvarende gjelder dersom levering til avtalt tid var
+            avgjørende for inngåelsen av avtalen, eller dersom kjøperen har
+            underrettet selger om at leveringstidspunktet er avgjørende.
+          </p>
+          <p>
+            Leveres tingen etter tilleggsfristen forbrukeren har satt eller
+            etter leveringstidspunktet som var avgjørende for inngåelsen av
+            avtalen, må krav om heving gjøres gjeldende innen rimelig tid etter
+            at kjøperen fikk vite om leveringen.
+          </p>
+          <h3>Erstatning</h3>
+          <p>
+            Kjøperen kan kreve erstatning for lidt tap som følge av
+            forsinkelsen. Dette gjelder imidlertid ikke dersom selgeren godtgjør
+            at forsinkelsen skyldes hindring utenfor selgers kontroll som ikke
+            med rimelighet kunne blitt tatt i betraktning på avtaletiden,
+            unngått, eller overvunnet følgene av.
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h2>
+          10. Mangel ved varen - kjøperens rettigheter og reklamasjonsfrist
+        </h2>
+
+        <div>
+          <p>
+            Hvis det foreligger en mangel ved varen må kjøper innen rimelig tid
+            etter at den ble oppdaget eller burde ha blitt oppdaget, gi selger
+            melding om at han eller hun vil påberope seg mangelen. Kjøper har
+            alltid reklamert tidsnok dersom det skjer innen 2 mnd. fra mangelen
+            ble oppdaget eller burde blitt oppdaget. Reklamasjon kan skje senest
+            to år etter at kjøper overtok varen. Dersom varen eller deler av den
+            er ment å vare vesentlig lenger enn to år, er reklamasjonsfristen
+            fem år.
+          </p>
+          <p>
+            Dersom varen har en mangel og dette ikke skyldes kjøperen eller
+            forhold på kjøperens side, kan kjøperen i henhold til reglene i
+            forbrukerkjøpsloven kapittel 6 etter omstendighetene{" "}
+            <em>holde kjøpesummen tilbake</em>, velge mellom <em>retting </em>og{" "}
+            <em>omlevering</em>, kreve <em>prisavslag</em>, kreve avtalen hevet
+            og/eller kreve <em>erstatning&nbsp; </em>fra selgeren.
+          </p>
+          <p>Reklamasjon til selgeren bør skje skriftlig.</p>
+          <h3>Retting eller omlevering</h3>
+          <p>
+            Kjøperen kan velge mellom å kreve mangelen rettet eller levering av
+            tilsvarende ting. Selger kan likevel motsette seg kjøperens krav
+            dersom gjennomføringen av kravet er umulig eller volder selgeren
+            urimelige kostnader. Retting eller omlevering skal foretas innen
+            rimelig tid. Selger har i utgangspunktet ikke rett til å foreta mer
+            enn to avhjelpsforsøk for samme mangel.
+          </p>
+          <h3>Prisavslag</h3>
+          <p>
+            Kjøper kan kreve et passende prisavslag dersom varen ikke blir
+            rettet eller omlevert. Dette innebærer at forholdet mellom nedsatt
+            og avtalt pris svarer til forholdet mellom tingens verdi i
+            mangelfull og kontraktsmessig stand. Dersom særlige grunner taler
+            for det, kan prisavslaget i stedet settes lik mangelens betydning
+            for kjøperen.
+          </p>
+          <h3>Heving</h3>
+          <p>
+            Dersom varen ikke er rettet eller omlevert, kan kjøperen også heve
+            kjøpet når mangelen ikke er uvesentlig.
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h2>11. Selgerens rettigheter ved kjøperens mislighold</h2>
+
+        <div>
+          <p>
+            Dersom kjøperen ikke betaler eller oppfyller de øvrige pliktene
+            etter avtalen eller loven, og dette ikke skyldes selgeren eller
+            forhold på selgerens side, kan selgeren i henhold til reglene i
+            forbrukerkjøpsloven kapittel 9 etter omstendighetene <em>holde</em>{" "}
+            <em>varen tilbake</em>, kreve <em>oppfyllelse </em>av avtalen, kreve
+            avtalen <em>hevet </em>samt kreve <em>erstatning </em>fra kjøperen.
+            Selgeren vil også etter omstendighetene kunne kreve{" "}
+            <em>renter ved forsinket betaling, inkassogebyr</em> og et rimelig{" "}
+            <em>gebyr ved uavhentede varer</em>.
+          </p>
+          <h3>Oppfyllelse</h3>
+          <p>
+            Selger kan fastholde kjøpet og kreve at kjøperen betaler
+            kjøpesummen. Er varen ikke levert, taper selgeren sin rett dersom
+            han venter urimelig lenge med å fremme kravet.
+          </p>
+          <h3>Heving</h3>
+          <p>
+            Selger kan heve avtalen dersom det foreligger vesentlig
+            betalingsmislighold eller annet vesentlig mislighold fra kjøperens
+            side. Selger kan likevel ikke heve dersom hele kjøpesummen er
+            betalt. Fastsetter selger en rimelig tilleggsfrist for oppfyllelse
+            og kjøperen ikke betaler innen denne fristen, kan selger heve
+            kjøpet.
+          </p>
+          <h3>Renter ved forsinket betaling/inkassogebyr</h3>
+          <p>
+            Dersom kjøperen ikke betaler kjøpesummen i henhold til avtalen, kan
+            selger kreve renter av kjøpesummen etter forsinkelsesrenteloven. Ved
+            manglende betaling kan kravet, etter forutgående varsel, bli sendt
+            til Kjøper kan da bli holdt ansvarlig for gebyr etter inkassoloven.
+          </p>
+          <h3>Gebyr ved uavhentede ikke-forskuddsbetalte varer</h3>
+          <p>
+            Dersom kjøperen unnlater å hente ubetalte varer, kan selger belaste
+            kjøper med et gebyr. Gebyret skal maksimalt dekke selgerens faktiske
+            &nbsp;utlegg for å levere varen til kjøperen. Et slikt gebyr kan
+            ikke belastes kjøpere under 18 &nbsp;år.
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h2>12. Garanti</h2>
+
+        <div>
+          <p>
+            Garanti som gis av selgeren eller produsenten, gir kjøperen
+            rettigheter i tillegg til de kjøperen allerede har etter ufravikelig
+            lovgivning. En garanti innebærer dermed ingen begrensninger i
+            kjøperens rett til reklamasjon og krav ved forsinkelse eller mangler
+            etter punkt 9 og 10.
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h2>13. Personopplysninger</h2>
+
+        <div>
+          <p>
+            Behandlingsansvarlig for innsamlede personopplysninger er selger.
+            Med mindre kjøperen samtykker til noe annet, kan selgeren, i tråd
+            med personopplysningsloven, kun innhente og lagre de
+            personopplysninger som er nødvendig for at selgeren skal kunne
+            gjennomføre forpliktelsene etter avtalen. Kjøperens
+            personopplysninger vil kun bli utlevert til andre hvis det er
+            nødvendig for at selger skal få gjennomført avtalen med kjøperen,
+            eller i lovbestemte tilfelle.
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h2>14. Konfliktløsning</h2>
+
+        <div>
+          <p>
+            Klager rettes til selger innen rimelig tid, jf. punkt 9 og 10.
+            Partene skal forsøke å løse eventuelle tvister i minnelighet. Dersom
+            dette ikke lykkes, kan kjøperen ta kontakt med Forbrukerrådet for
+            mekling. Forbrukerrådet er tilgjengelig &nbsp;på &nbsp;telefon
+            23&nbsp; 400&nbsp; 500&nbsp; eller{" "}
+            <a
+              target="_blank"
+              href="http://www.forbrukerradet.no/"
+              rel="noopener"
+            >
+              www.forbrukerradet.no.
+            </a>
+          </p>
+          <p>
+            Europa-Kommisjonens klageportal kan også brukes hvis du ønsker å
+            inngi en klage. Det er særlig relevant, hvis du er forbruker bosatt
+            i et annet EU-land. Klagen inngis her:&nbsp;
+            <a href="http://ec.europa.eu/odr">http://ec.europa.eu/odr</a>.
+          </p>
+          <p>&nbsp;</p>
+        </div>
+      </section>
     </div>
   );
 }
